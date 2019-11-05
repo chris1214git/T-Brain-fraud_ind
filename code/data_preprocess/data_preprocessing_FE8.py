e1cba3da8eac04069673be086ddbab95fbfcc87f
chris1214git <b04901068@ntu.edu.tw>
2019-11-05 15:23:28 +0800

    FE8

--- code/data_preprocess/data_preprocessing_FE8.py
+++ code/data_preprocess/data_preprocessing_FE8.py
@@ -12,7 +12,23 @@ print(all_data.head())
 raw_col_num = all_data.shape[1]
 
 
+tmp_df = all_data.groupby(['bacno','stocn'])['locdt'].max().reset_index()
+tmp_df.columns = ['bacno','stocn','locdt']
+tmp_df['bacno_stocn_lastday']=1
+all_data = pd.merge(all_data,tmp_df,how='left',on=['bacno','stocn','locdt'])
+all_data['bacno_stocn_lastday'] = all_data['bacno_stocn_lastday'].fillna(value=0)
 
+tmp_df = all_data.groupby(['bacno','scity'])['locdt'].max().reset_index()
+tmp_df.columns = ['bacno','scity','locdt']
+tmp_df['bacno_scity_lastday']=1
+all_data = pd.merge(all_data,tmp_df,how='left',on=['bacno','scity','locdt'])
+all_data['bacno_scity_lastday'] = all_data['bacno_scity_lastday'].fillna(value=0)
+
+tmp_df = all_data.groupby(['bacno','csmcu'])['locdt'].max().reset_index()
+tmp_df.columns = ['bacno','csmcu','locdt']
+tmp_df['bacno_csmcu_lastday']=1
+all_data = pd.merge(all_data,tmp_df,how='left',on=['bacno','csmcu','locdt'])
+all_data['bacno_csmcu_lastday'] = all_data['bacno_csmcu_lastday'].fillna(value=0)
 
 
 # write file
